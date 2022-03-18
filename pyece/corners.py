e5ee9519265f56ef2a34818dc3c5b44d48c013c5
Vladislav A. Proskurov <rilshok@pm.me>
2022-03-18 11:15:44 +0700

    corners base

--- pyece/corners.py
+++ pyece/corners.py
@@ -1,8 +1,37 @@
+from itertools import product
+from typing import Sequence, Tuple, Union
 
+import numpy as np
+
+Point = Sequence[Union[float, int]]
+LikeCorners = Union[np.ndarray, Sequence[Point]]
 
 class Corners:
-    def __init__(self):
-        pass
+    def __init__(self, corners: LikeCorners):
+        corners = np.asarray(corners)
+        n, p = corners.shape
+        if 2**p != n:
+            msg = ""
+            raise ValueError(msg)
+        self._corners = np.asarray(corners)
 
-    def __repr__(self):
+    def copy(self) -> "Corners":
+        return Corners(self._corners)
+
+    def dim(self):
+        return self._corners.shape[1]
+
+    @staticmethod
+    def product(shape: Tuple) -> "Corners":
+        masks = list(product((False, True), repeat=len(shape)))
+        corners = [[s if c else 0 for c, s in zip(mask, shape)] for mask in masks]
+        return Corners(corners)
+
+    @property
+    def сentre(self):
+        return self._corners.mean(0)
+
+    def __getitem__(self, index):
         pass
+    # def __repr__(self):
+    #     return

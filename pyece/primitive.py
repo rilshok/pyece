8440b616472a7e83e3d0b6d4196582209e2d6ed9
Vladislav A. Proskurov <rilshok@pm.me>
2022-03-18 11:52:28 +0700

    + abstract property

--- pyece/primitive.py
+++ pyece/primitive.py
@@ -1,6 +1,34 @@
 
-class Point:
-    pass
+from abc import abstractmethod
+
+from abc import ABC, abstractmethod
+from typing import Any, Sequence, Union
+import numpy as np
+
+
+LikeProperty = Union["Property", float, int]
+LikePoint = Sequence[LikeProperty]
+
+
+class Property(ABC):
+    @abstractmethod
+    def get(self)->Any:
+        return NotImplemented
+
+    @staticmethod
+    def value(instance: LikeProperty):
+        if isinstance(instance, Property):
+            return instance.get()
+        return instance
+
+
+class Point(Property):
+    def __init__(self, point: LikePoint):
+        self._point = list(point)
+
+    def get(self) -> np.ndarray:
+        return np.asanyarray([Property.value(p) for p in self._point])
+
 
 class Vector:
     pass

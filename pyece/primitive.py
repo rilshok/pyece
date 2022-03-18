6f8f327343ae0677155756d3ae2e3e37cf35afe4
Vladislav A. Proskurov <rilshok@pm.me>
2022-03-18 13:07:37 +0700

    + Iter iterable property

--- pyece/primitive.py
+++ pyece/primitive.py
@@ -1,7 +1,7 @@
-from abc import abstractmethod
-
+import types
 from abc import ABC, abstractmethod
-from typing import Any, Callable, Sequence, Union
+from typing import Any, Callable, Iterable, Sequence, Union
+
 import numpy as np
 
 LikeProperty = Union["Property", float, int, str]
@@ -32,7 +32,7 @@ def as_property(value: LikeProperty) -> Property:
     return ConstantProperty(value)
 
 
-class Convertible(Property):
+class Convert(Property):
     def __init__(self, p: LikeProperty, fn: Callable, *args, **kwargs):
         assert callable(fn)
         self._original = as_property(p)
@@ -43,6 +43,20 @@ class Convertible(Property):
     def get(self) -> Any:
         return self._fn(self._original.value, *self._args, **self._kwargs)
 
+class Iter(Property):
+    def __init__(self, seq: Iterable):
+        self._source = seq
+        self._iter = iter(self._source)
+
+    def _next(self):
+        try:
+            return next(self._iter)
+        except StopIteration:
+            self._iter = iter(self._source)
+            return next(self._iter)
+
+    def get(self) -> Any:
+        return self._next()
 
 class RandomUniform(Property):
     def __init__(self, low=0.0, high=1.0):
@@ -65,5 +79,9 @@ class Vector:
     pass
 
 
-class Angle:
-    pass
+class Angle(Property):
+    def __init__(self, p: LikeProperty):
+        self._original = as_property(p)
+
+    def get(self):
+        return self._original.value % (2 * np.pi)

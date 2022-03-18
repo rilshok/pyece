1cf54567d9109a1b8a4df7b652807708ec9a98fa
Vladislav A. Proskurov <rilshok@pm.me>
2022-03-18 11:21:29 +0700

    test product

---
+++ tests/test_corners.py
@@ -0,0 +1,15 @@
+from pytest import mark
+from pyece.corners import Corners
+
+@mark.parametrize(
+    "shape",
+    [
+        (1,2),
+        (1,2,3),
+        (1,2,3,4)
+    ],
+)
+def test_corners_product(shape):
+    cs = Corners.product(shape)
+    assert cs.dim == len(shape)
+    assert cs.сentre.mean() == sum(shape)/len(shape)/2

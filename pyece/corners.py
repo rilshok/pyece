4d58f02dd5e81ae0cc2aab6404959182c41f3217
Vladislav A. Proskurov <rilshok@pm.me>
2022-03-30 16:38:13 +0700

    init transforms

--- pyece/corners.py
+++ pyece/corners.py
@@ -1,48 +1,22 @@
-from abc import ABC, abstractclassmethod, abstractmethod
 from itertools import product
-from typing import Sequence, Tuple, Union
+from typing import Tuple
 
 import numpy as np
 
-from .primitive import Property
-
-Point = Sequence[Union[float, int]]
-LikeCorners = Union[np.ndarray, Sequence[Point]]
+from .primitive import PointCloud
 
 
-class Corners(Property):
-    def __init__(self, corners: LikeCorners):
-        corners = np.asarray(corners)
+class Corners(PointCloud):
+    def get(self) -> np.ndarray:
+        corners = super().get()
         n, p = corners.shape
         if 2 ** p != n:
             msg = ""
             raise ValueError(msg)
-        self._corners = np.asarray(corners)
-
-    def get(self):
-        return self._corners
-
-    def copy(self) -> "Corners":
-        return Corners(self._corners)
+        return corners
 
     @staticmethod
     def product(shape: Tuple) -> "Corners":
         masks = list(product((False, True), repeat=len(shape)))
         corners = [[s if c else 0 for c, s in zip(mask, shape)] for mask in masks]
         return Corners(corners)
-
-    @property
-    def dim(self) -> int:
-        return self._corners.shape[1]
-
-    @property
-    def centre(self) -> np.ndarray:
-        return self._corners.mean(0)
-
-    def __getitem__(self, index):
-        pass
-
-    def __repr__(self) -> str:
-        d = self.dim
-        c = tuple(self.centre)
-        return f"Corners{d}d[сentre={c}]"
--- pyece/primitive.py
+++ pyece/primitive.py
@@ -32,16 +32,25 @@ def as_property(value: LikeProperty) -> Property:
     return ConstantProperty(value)
 
 
-class Convert(Property):
-    def __init__(self, p: LikeProperty, fn: Callable, *args, **kwargs):
+class Source(Property):
+    def __init__(self, fn: Callable, *args, **kwargs):
         assert callable(fn)
-        self._original = as_property(p)
         self._fn = fn
         self._args = args
         self._kwargs = kwargs
 
     def get(self) -> Any:
+        return self._fn(*self._args, **self._kwargs)
+
+
+class Convert(Source):
+    def __init__(self, p: LikeProperty, fn: Callable, *args, **kwargs):
+        self._original = as_property(p)
+        super().__init__(fn, *args, **kwargs)
+
+    def get(self) -> Any:
         return self._fn(self._original.value, *self._args, **self._kwargs)
+
 
 class Iter(Property):
     def __init__(self, seq: Iterable):
@@ -57,6 +66,7 @@ class Iter(Property):
 
     def get(self) -> Any:
         return self._next()
+
 
 class RandomUniform(Property):
     def __init__(self, low=0.0, high=1.0):
@@ -75,13 +85,48 @@ class Point(Property):
         return np.asarray([p.value for p in self._point])
 
 
-class Vector:
-    pass
+LikePoint = Union[Point, PropertySequence]
 
 
-class Angle(Property):
-    def __init__(self, p: LikeProperty):
-        self._original = as_property(p)
+def as_point(value: LikePoint) -> Point:
+    if isinstance(value, Point):
+        return value
+    return Point(value)
 
-    def get(self):
-        return self._original.value % (2 * np.pi)
+
+class PointCloud(Point):
+    def __init__(self, points: Sequence[LikePoint]):
+        super().__init__([as_point(p) for p in points])
+
+
+class PointTransform(ABC):
+    @abstractmethod
+    def transform(self, point: np.ndarray) -> np.ndarray:
+        return NotImplemented
+
+    def __call__(self, obj: LikePoint) -> Point:
+        p = as_point(obj).value
+        t = self.transform(p)
+        return as_point(t)
+
+
+class PointShift(PointTransform):
+    def __init__(self, shift: LikePoint):
+        self._shift = as_point(shift)
+
+    def transform(self, point: np.ndarray) -> np.ndarray:
+        return point + self._shift.value
+
+
+from .math.rotate import rotate
+
+
+class PointRotate(PointTransform):
+    def __init__(self, pivot: LikePoint, angle: LikeProperty):
+        self._pivot = as_point(pivot)
+        self._angle = as_property(angle)
+
+    def transform(self, point: np.ndarray) -> np.ndarray:
+        pivot = self._pivot.value
+        angle = np.asarray(self._angle.value).reshape(-1) % (2 * np.pi)
+        return rotate(pivot, point, angle)

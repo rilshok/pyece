09d595a5a10766bcd5a5753ce83cedd57be96912
Vladislav A. Proskurov <rilshok@pm.me>
2022-04-02 16:25:34 +0700

    make corners

--- tests/test_corners.py
+++ tests/test_corners.py
@@ -1,4 +1,5 @@
 from pytest import mark
+import pytest
 from pyece.corners import Corners
 
 
@@ -8,5 +9,21 @@ from pyece.corners import Corners
 )
 def test_corners_product(shape):
     cs = Corners.product(shape).value
-    assert cs.shape[0] == 2**len(shape)
+    assert cs.shape[0] == 2 ** len(shape)
     assert cs.mean() == sum(shape) / len(shape) / 2
+    assert (cs[0] == [0]*len(shape)).all()
+    assert (cs[-1] == shape).all()
+    assert set(cs.flatten()) == {0, *shape}
+
+
+def test_corners():
+    assert Corners([[]]).value.shape == (1, 0)
+    with pytest.raises(ValueError):
+        Corners([[1]]).value
+    with pytest.raises(ValueError):
+        Corners([[0, 0]]*3).value
+    assert Corners([[0, 0]]*4).value.shape == (4,2)
+    with pytest.raises(ValueError):
+        Corners([[0, 0]]*8).value
+    assert Corners([[0]*3]*8).value.shape == (8, 3)
+    assert Corners([[0]*4]*16).value.shape == (16, 4)

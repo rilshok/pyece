518c0f1d79b505678407f87694693cdb3e556742
Vladislav A. Proskurov <rilshok@pm.me>
2022-04-02 16:00:05 +0700

    fix test

--- tests/test_corners.py
+++ tests/test_corners.py
@@ -1,15 +1,12 @@
 from pytest import mark
 from pyece.corners import Corners
 
+
 @mark.parametrize(
     "shape",
-    [
-        (1,2),
-        (1,2,3),
-        (1,2,3,4)
-    ],
+    [(1, 2), (1, 2, 3), (1, 2, 3, 4)],
 )
 def test_corners_product(shape):
-    cs = Corners.product(shape)
-    assert cs.dim == len(shape)
-    assert cs.centre.mean() == sum(shape)/len(shape)/2
+    cs = Corners.product(shape).value
+    assert cs.shape[0] == 2**len(shape)
+    assert cs.mean() == sum(shape) / len(shape) / 2

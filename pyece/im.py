e48c9c666a2e1b0b60407e0ad64282cebb88d982
Vladislav A. Proskurov <rilshok@pm.me>
2022-05-12 15:06:29 +0300

    + h5 support

--- pyece/im.py
+++ pyece/im.py
@@ -1,5 +1,8 @@
+import typing as tp
+
+import h5py as h5
 import numpy as np
-from typing import Tuple
+
 
 def linspace(start, end, count: int):
     # NOTE: np.linspace works ~ 8 times faster
@@ -10,7 +13,7 @@ def linspace(start, end, count: int):
 
 
 # TODO: do it asynchronously
-def meshcorners(corners: np.ndarray, grid: Tuple):
+def meshcorners(corners: np.ndarray, grid: tp.Tuple):
     c = len(corners) // 2
     if c == 0:
         return corners[0]
@@ -20,11 +23,15 @@ def meshcorners(corners: np.ndarray, grid: Tuple):
     )
 
 
-def cutpatch(data: np.ndarray, corners: np.ndarray, grid: tuple, fill=None):
+def cutpatch(
+    data: tp.Union[np.ndarray, h5.Dataset], corners: np.ndarray, grid: tuple, fill=None
+) -> np.ndarray:
     mesh = np.asarray(meshcorners(corners, grid)).round()
     idx = np.rollaxis(mesh.astype(int), -1)
     d = idx.shape[0]
     ring_idx = idx % np.reshape(data.shape[:d], (d, *[1] * d))
+    if isinstance(data, h5.Dataset):
+        data = data[:]
     patch = data[tuple(ring_idx)]
     if fill is not None:
         fill_idx = (idx != ring_idx).any(0)

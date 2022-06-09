d5aa6bd16b8a54707db137bc5df767b03e004632
rilshok <rilshok@pm.me>
2022-06-09 18:28:18 +0400

    new way for patching

--- pyece/im.py
+++ pyece/im.py
@@ -1,10 +1,10 @@
-import typing as tp
+import itertools
 
 import h5py as h5  # type: ignore
 import numpy as np
-from .core.property import Corners
 
-import itertools
+from .core import typing as tp
+from .core.property import Corners
 
 
 def linspace(start, end, count: int):
@@ -16,20 +16,21 @@ def linspace(start, end, count: int):
 
 
 # TODO: do it asynchronously
-def meshcorners(corners: np.ndarray, grid: tp.Tuple):
+def meshcorners(corners: tp.NDArray, grid: tp.IntTuple):
     c = len(corners) // 2
     if c == 0:
         return corners[0]
-
     return linspace(
-        meshcorners(corners[:c], grid[1:]), meshcorners(corners[c:], grid[1:]), grid[0]
+        start=meshcorners(corners[:c], grid[1:]),
+        end=meshcorners(corners[c:], grid[1:]),
+        count=grid[0],
     )
 
 
 def cutpatch(
-    data: tp.Union[np.ndarray, h5.Dataset],
-    corners: np.ndarray,
-    grid: tp.Tuple[int, ...],
+    data: tp.Union[tp.NDArray, h5.Dataset],
+    corners: tp.NDArray,
+    grid: tp.IntTuple,
     fill: tp.Any = None,
 ) -> np.ndarray:
     mesh = np.asarray(meshcorners(corners, grid)).round()
@@ -48,62 +49,58 @@ def cutpatch(
 class ByPatchWrapper:
     def __init__(
         self,
-        func: tp.Callable[[np.ndarray], tp.Any],
-        size: tp.Union[np.ndarray, tp.Tuple[int, ...]],
-        spacing: tp.Union[np.ndarray, tp.Tuple[float, ...]] = None,
-        grid: tp.Union[np.ndarray, tp.Tuple[float, ...]] = None,
+        func: tp.Callable[[tp.NDArray], tp.NDArray],
+        size: tp.IntTuple,
+        spacing: tp.FloatTuple = None,
+        grid: tp.IntTuple = None,
     ):
         assert callable(func)
         self._wrapper = func
-        dim = len(size)
-        size = np.asarray(size)
-        spacing = np.asarray(spacing or [1.0] * dim)
 
+        self._dim = len(size)
+        self._size = np.asarray(size)
+
+        self._spacing = np.asarray(spacing or [1.0] * self._dim)
+        assert len(self._spacing) == self._dim
+
+        self._volume = self._size * self._spacing
+
+        assert (grid is None) or (len(grid) == self._dim)
+        self._grid = None
         if grid is not None:
-            grid = np.asarray(grid).astype(int)
-
-        assert len(spacing) == dim
-        assert (grid is None) or (len(grid) == dim)
-
-        self._dim = dim
-        self._size = volume = size
-        if spacing is not None:
-            volume = volume * spacing
-        self._volume = volume
-        self._corners = Corners.product(tuple(volume)).value
-
-        self._grid = grid
+            assert len(grid) == self._dim
+            self._grid = np.asarray(grid).astype(int)
 
     def __call__(
         self,
-        data: np.ndarray,
-        spacing: tp.Union[np.ndarray, tp.Tuple[float, ...]] = None,
-        grid: tp.Union[np.ndarray, tp.Tuple[float, ...]] = None,
+        data: tp.NDArray,
+        spacing: tp.FloatTuple = None,
+        grid: tp.IntTuple = None,
         **kwargs,
     ) -> np.ndarray:
-        size = np.asarray(data.shape[: self._dim])
-        if spacing is not None:
-            size = size * np.asarray(spacing)
-
+        shape = np.asarray(data.shape[: self._dim])
+        spacing = np.asarray(spacing or [1.0] * self._dim)
+        volume = shape * spacing
+        # NOTE: resize data to required spacing
+        if not np.all(self._spacing == spacing):
+            data = cutpatch(
+                data=data,
+                corners=Corners.product(shape).value - 0.5,
+                grid=np.round(volume / self._spacing).astype(int),
+            )
         grid = np.asarray(
-            grid or self._grid or np.floor((size / self._volume)), dtype="int"
+            grid or self._grid or np.floor((volume / self._volume)),
+            dtype="int",
         )
+
         ids = np.asarray(list(itertools.product(*map(range, grid))))
-        shift = self._volume + ((size - (grid * self._volume)) / (grid - 1))
-        shifts = ids * shift
+        shift = self._volume + ((volume - (grid * self._volume)) / (grid - 1))
+        shifts = np.round((ids * shift)).astype(int)
 
         wrapped = list()
         for shift in shifts:
-            corners = self._corners + shift
-            if spacing is not None:
-                corners = corners / spacing
-            corners = corners - 0.5
-            patch = cutpatch(
-                data=data,
-                grid=tuple(self._size),
-                corners=corners,
-            ).astype(np.float32)
-
+            idx = tuple(slice(s, e, 1) for s, e in zip(shift, shift + self._size))
+            patch = data[idx]
             wrapped_patch = self._wrapper(patch, **kwargs)
             wrapped.append(wrapped_patch)
 
